75de6b87abef6911fe9fd235d105228b86b7ea61
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-08-29 01:41:15 -0500

    Add files via upload

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la temperatura en grados Fahrenheit:");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+            double celsius = ( fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine($"La temperatura en grados Celsius es: {celsius:F2}");
+        }
+    }
+}
